33c811149444a704dfeda9ef930487d17b2fa023
NGC <jngaravitoc@email.arizona.edu>
2022-07-11 07:03:10 -0400

    Code to query and analyze Illustris

---
+++ plot_accretion_history.py
@@ -0,0 +1,206 @@
+"""
+Script to analyze MW analogues with three mergers > 1/10. 
+
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import illustris_python as il
+
+
+def plot_accretion_histories(bp, groupids, figname):
+    """
+
+    """
+
+    fields = ['SubhaloMass','SubfindID','SnapNum']
+    for n in groupids:
+      lb_arr = []
+      tree = il.sublink.loadTree(bp, 135, n, fields=fields,onlyMPB=True)
+      snap_t = tree["SnapNum"]
+      for i in snap_t:
+        lb_arr.append(lb_time[np.where(snap==i)[0]])
+      plt.plot(lb_arr, tree['SubhaloMass'],'-', alpha=0.4, lw=0.5)
+      plt.fill_betweenx(np.logspace(-1.5, 3, 10), np.ones(10)*9, np.ones(10)*12, color='C0', alpha=0.3)
+      plt.fill_betweenx(np.logspace(-1.5, 3, 10), np.ones(10)*6, np.ones(10)*8, color='C1', alpha=0.3)
+      plt.fill_betweenx(np.logspace(-1.5, 3, 10), np.ones(10)*1.5, np.ones(10)*3, color='C2', alpha=0.3)
+      plt.yscale('log')
+      plt.xlabel('$Lookback   t [Gyr]$')
+      plt.ylabel('Total Subhalo Mass [code units]')
+      plt.ylim(1, 3E2)
+    plt.savefig(figname, bbox_inches='tight')
+    plt.close()
+
+
+def get_analogues(sat1_lbt, sat2_lbt, sat3_lbt, mrat1, mrat2, mrat3, id_halos, analogue, write_data=False):
+    """
+    By definiton sat1 is the most recent merger, sat2 the intermediate and sat3
+    the oldest merger. 
+
+    """
+    GSE = 10
+    SAG = 7
+    LMC = 2.2
+   
+    if analogue == "LMC":
+        mw_analogues = np.where(((sat1_lbt < LMC+0.7) & (sat1_lbt > LMC-0.7)))
+        filename = "LMC_analogues.txt"
+
+    elif analogue == "LMC+SAG":
+        mw_analogues = np.where(((sat1_lbt < LMC+0.7) & (sat1_lbt > LMC-0.7)) & ((sat2_lbt < SAG+1) & (sat2_lbt>SAG-1)))
+        filename = "LMC_SAG_analogues.txt"
+
+    elif analogue == "LMC+GSE":
+        mw_analogues = np.where(((sat1_lbt < LMC+0.7) & (sat1_lbt > LMC-0.7)) & ((sat3_lbt < GSE+1) & (sat3_lbt > GSE-1)))
+        filename = "LMC_GSE_analogues.txt"
+    
+    elif analogue == "LMC+SAG+GSE":
+        mw_analogues = np.where(((sat1_lbt < LMC+0.7) & (sat1_lbt > LMC-0.7)) & ((sat2_lbt < SAG+1) & (sat2_lbt>SAG-1)) & ((sat3_lbt < GSE+1) & (sat3_lbt > GSE-1)))
+        filename = "LMC_SAG_GSE_analogues.txt"
+    
+    elif analogue == "SAG":
+        mw_analogues = np.where(((sat2_lbt < SAG+1) & (sat2_lbt>SAG-1)))
+        filename = "SAG_analogues.txt"
+    
+    elif analogue == "SAG+GSE":
+        mw_analogues = np.where(((sat2_lbt < SAG+1) & (sat2_lbt>SAG-1)) & ((sat3_lbt < GSE+1) & (sat3_lbt > GSE-1)))
+        filename = "SAG_GSE_analogues.txt"
+    
+    elif analogue == "GSE":
+        mw_analogues = np.where(((sat3_lbt < GSE+1) & (sat3_lbt > GSE-1)))
+        filename = "GSE_analogues.txt"
+       
+    N_analogues = len(mw_analogues[0])
+    IDhalos = id_halos[mw_analogues]
+    lbtimes = [sat1_lbt[mw_analogues], sat2_lbt[mw_analogues], sat3_lbt[mw_analogues]]
+    mratios = [mrat1[mw_analogues], mrat2[mw_analogues], mrat3[mw_analogues]]
+   
+
+    if write_data==True :         
+        head = "ID host halo, mass ratio sat1, mass ratio sat 2, mass ratio sat 3, looback t sat1, lookback t sat 2, lookback sat 3"
+        np.savetxt(filename, np.array([IDhalos, mratios[0], mratios[1],
+              mratios[2], lbtimes[0], lbtimes[1], lbtimes[2]]).T,
+              fmt=['%d','%.4f', '%.4f', "%.4f", "%.4f", "%.4f", "%.4f"],
+              header=head )
+
+
+    return N_analogues, IDhalos, mratios, lbtimes
+
+
+####### DATA ###########
+
+# Illustris sims path
+basePath = '/mnt/home/nico/ceph/illutris/Illustris-3/output'
+
+# MW analogues with 3 mergers 
+mw_analogues = np.loadtxt("halos_three_mergers.txt")
+GroupFirstSub = mw_analogues[:,0]
+
+nsnap_sat1 = mw_analogues[:,5]
+nsnap_sat2 = mw_analogues[:,6]
+nsnap_sat3 = mw_analogues[:,7]
+mrat1 = mw_analogues[:,14]
+mrat2 = mw_analogues[:,15]
+mrat3 = mw_analogues[:,16]
+
+# snap, redshift age lookback time
+snap_z = np.loadtxt("snaps_z_age.txt")
+snap = snap_z[:,0]
+z = snap_z[:,1]
+age = snap_z[:,2]
+lb_time = snap_z[:,3]
+
+
+# Defining lookback times of 3 mergers in the MW
+
+#GSE = 10
+#SAG = 7
+#LMC = 2.2
+
+
+# Getting lookback times for satellites and sorting them 
+
+sat1lbt=np.zeros_like(nsnap_sat1)
+sat2lbt=np.zeros_like(nsnap_sat1)
+sat3lbt=np.zeros_like(nsnap_sat1)
+
+sat1_mrat=np.zeros_like(nsnap_sat1)
+sat2_mrat=np.zeros_like(nsnap_sat1)
+sat3_mrat=np.zeros_like(nsnap_sat1)
+
+## Sort mergers as a function of loolback time
+
+# Sat1 -> most recent merger
+# Sat3 -> oldest merger
+
+for j in range(len(nsnap_sat1)):
+  s1 = lb_time[np.where(snap==nsnap_sat1[j])[0]]
+  s2 = lb_time[np.where(snap==nsnap_sat2[j])[0]]
+  s3 = lb_time[np.where(snap==nsnap_sat3[j])[0]]
+  
+  all_s = np.array([s1, s2, s3])
+
+  sat1lbt[j] = np.sort(all_s)[0]
+  sat2lbt[j] = np.sort(all_s)[1]
+  sat3lbt[j] = np.sort(all_s)[2]
+  
+  all_mrat = np.array([mrat1[j], mrat2[j], mrat3[j]])
+  sat1_mrat[j] = all_mrat[np.argsort(all_s)[0]]
+  sat2_mrat[j] = all_mrat[np.argsort(all_s)[1]]
+  sat3_mrat[j] = all_mrat[np.argsort(all_s)[2]]
+
+
+######## ANALOGUES  ########
+
+#LMC_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat, sat2_mrat,  sat3_mrat, GroupFirstSub, "LMC", write_data=True)
+#LMC_SAG_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat, sat2_mrat, sat3_mrat, GroupFirstSub, "LMC+SAG", write_data=True)
+#LMC_GSE_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat, sat2_mrat, sat3_mrat, GroupFirstSub, "LMC+GSE")
+#LMC_SAG_GSE_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat,  sat2_mrat, sat3_mrat, GroupFirstSub, "LMC+SAG+GSE", write_data=True)
+#SAG_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat, sat2_mrat, sat3_mrat, GroupFirstSub, "SAG", write_data=True)
+SAG_GSE_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat, sat2_mrat, sat3_mrat, GroupFirstSub, "SAG+GSE", write_data=True)
+#GSE_analogues = get_analogues(sat1lbt, sat2lbt, sat3lbt, sat1_mrat, sat2_mrat, sat3_mrat, GroupFirstSub, "GSE")
+
+
+######### FIGURES #############
+
+# Histograms of when all the three mergers happen 
+
+"""
+fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+ax[0].hist(sat1lbt)
+ax[1].hist(sat2lbt)
+ax[2].hist(sat3lbt)
+ax[1].set_xlabel("Lookback time [Gyr]")
+ax[0].set_ylabel("N MW Analogues")
+ax[0].set_title("3rd merger")
+ax[1].set_title("2nd merger")
+ax[2].set_title("1st merger")
+
+plt.savefig("histograms_lookback_times_satellites.png", bbox_inches='tight')
+plt.close()
+"""
+
+
+
+#LMC_SAG_analogues[0]
+#LMC_SAG_analogues[0]
+#LMC_SAG_analogues[0]
+
+sat1_lbt = SAG_GSE_analogues[3][0]
+sat2_lbt = SAG_GSE_analogues[3][1]
+sat3_lbt = SAG_GSE_analogues[3][2]
+
+fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+ax[0].hist(sat1_lbt)
+ax[1].hist(sat2_lbt)
+ax[2].hist(sat3_lbt)
+ax[1].set_xlabel("Lookback time [Gyr]")
+ax[0].set_title("3rd merger")
+ax[1].set_title("2nd merger")
+ax[2].set_title("1st merger")
+plt.savefig("histograms_lookback_times_sag_gse_analogues.png", bbox_inches='tight')
+plt.close()
+
+
+
---
+++ subhalos_of_halo.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import illustris_python as il
+from illustris_python.sublink import maxPastMass
+from illustris_python.util import partTypeNum 
+from illustris_python.groupcat import loadHeader
+
+# 1. Get all halos with three mergers
+# 2. Find mergers max mass and snap at which they have max mass, ids in the halo
+# catalogue. 
+# 3. Find times and snaps where the halos enter the halo and when they merge,
+# ids in halo catalgue.
+
+
+def get_redshifts(basePath, max_num_snapshots=136):
+    # modified from: https://www.tng-project.org/data/forum/topic/369/snapshots-and-redshifts/
+    redshifts = np.zeros((max_num_snapshots, 2), dtype='float32' )
+
+    for i in range(0, max_num_snapshots):
+        h = loadHeader(basePath,i)
+        redshifts[0, i] = h['Redshift']
+        redshifts[1, i] = i
+
+    np.savetxt("redshifts_snaps_illustris-3.txt", redshifts)
+    return 0
+
+def maxPastMass(tree, index, partType='stars'):
+      """ Get maximum past mass (of the given partType) along the main branch of
+      a subhalo
+      specified by index within this tree. """
+      ptNum = partTypeNum(partType)
+
+      branchSize = tree['MainLeafProgenitorID'][index] -  tree['SubhaloID'][index] + 1
+      masses = tree['SubhaloMassType'][index: index + branchSize, ptNum]
+      max_mass = np.argmax(masses)
+      return np.max(masses), index+max_mass
+
+def numMergersIDs(tree, minMassRatio=1e-10, massPartType='stars', index=0):
+    """ 
+    Calculate the number of mergers in this sub-tree (optionally above some mass ratio threshold). 
+    
+    Parameters:
+    -----------
+
+    index : 
+
+    """
+    # verify the input sub-tree has the required fields
+    reqFields = ['SubhaloID', 'NextProgenitorID', 'MainLeafProgenitorID',
+                 'FirstProgenitorID', 'SubhaloMassType']
+
+    if not set(reqFields).issubset(tree.keys()):
+        raise Exception('Error: Input tree needs to have loaded fields: '+', '.join(reqFields))
+
+    numMergers   = 0
+    invMassRatio = 1.0 / minMassRatio
+    id_subhalos  = []
+    snapnum_subhalos = []
+    snapnum_fp_max = []
+    ratios = []
+    id_subhalos_fpmax = []
+    # walk back main progenitor branch
+    rootID = tree['SubhaloID'][index]
+    fpID   = tree['FirstProgenitorID'][index]
+
+    while fpID != -1:
+        fpIndex = index + (fpID - rootID)
+        fpMass  = maxPastMass(tree, fpIndex, massPartType)
+
+        # explore breadth
+        npID = tree['NextProgenitorID'][fpIndex]
+        snapnum = tree['SnapNum'][fpIndex]
+        pos = tree['SubhaloPos'][fpIndex]
+
+        while npID != -1:
+            npIndex = index + (npID - rootID)
+            npMass, max_mass_index  = maxPastMass(tree, npIndex, massPartType)
+            subID = tree['SubfindID'][max_mass_index]
+            snapnum = tree['SnapNum'][max_mass_index]
+            # mass of FP branch at max sat. mass
+            branchSize = tree['MainLeafProgenitorID'][fpIndex] -  tree['SubhaloID'][fpIndex] + 1
+            snapnum_FP = tree['SnapNum'][fpIndex: fpIndex + branchSize]
+   
+            # index in tree of the FP at the time of max sat mass.
+            # First progenitor index
+            if len(snapnum_FP[snapnum_FP>=snapnum])>1:         
+                FP_max_index = np.argmin(snapnum_FP[snapnum_FP>=snapnum])+fpIndex 
+                fpMass_satmax = tree['SubhaloMassType'][FP_max_index, 4] # 4 is parttype
+                fpmax_snapnum = tree['SnapNum'][FP_max_index]
+                fpmax_subID = tree['SubfindID'][FP_max_index]
+        
+                # count if both masses are non-zero, and ratio exceeds threshold
+                if fpMass_satmax > 0.0 and npMass > 0.0 :
+                    ratio = npMass / fpMass_satmax
+
+                    if ratio >= minMassRatio and ratio <= invMassRatio:
+                        numMergers += 1
+                        id_subhalos.append(subID)
+                        snapnum_subhalos.append(snapnum)
+                        snapnum_fp_max.append(fpmax_snapnum)
+                        id_subhalos_fpmax.append(fpmax_subID)
+                        ratios.append(ratio)
+                npID = tree['NextProgenitorID'][npIndex]
+            else : 
+                npID = -1       
+
+        fpID = tree['FirstProgenitorID'][fpIndex]
+
+    return numMergers, id_subhalos, snapnum_subhalos, id_subhalos_fpmax,  snapnum_fp_max, ratios
+
+
+basePath = '/mnt/home/nico/ceph/illutris/Illustris-3/output'
+
+#get_redshifts(basePath)
+
+
+# 135 -> snapshot at z=0
+
+Grouphalos = il.groupcat.loadHalos(basePath,135,fields=['GroupFirstSub'])
+GroupFirstSub_Mass = il.groupcat.loadHalos(basePath,135,fields=['Group_M_Crit200'])
+Group_mass =GroupFirstSub_Mass * 1E10/0.704
+
+
+Groupsubhalos = il.groupcat.loadSubhalos(basePath,135,fields=['SubhaloMass'])
+
+
+# Select MW-like halos
+mw_cuts = np.where((Group_mass < 2E12) & (Group_mass > 0.5E12))
+
+print("N MW-like halos", len(mw_cuts[0]))
+
+Nmw_analogues = len(mw_cuts[0])
+
+
+"""
+ratio = 1/10.
+fields = ['SubhaloMass','SubfindID','SnapNum','SubhaloID','NextProgenitorID','MainLeafProgenitorID','FirstProgenitorID','SubhaloMassType','SubhaloPos']
+
+all_halos = np.zeros((Nmw_analogues, 19))
+k=0
+for i in range(Nmw_analogues):
+    tree = il.sublink.loadTree(basePath, 135, Grouphalos[mw_cuts][i], fields=fields)
+    numMergers = il.sublink.numMergers(tree, minMassRatio=ratio)
+    #ree2 = il.sublink.loadTree(basePath, 135, Grouphalos[mw_cuts][i], fields=fields, onlyMPB=True)
+    print("N mergers:", numMergers, "Halo ",  i)
+    if numMergers==3:
+        x = numMergersIDs(tree, minMassRatio=ratio)
+        if x[0]==3:
+            k+=1
+            all_halos[k] = np.array([Grouphalos[mw_cuts][i], x[0], x[1][0], x[1][1], x[1][2], x[2][0],
+              x[2][1], x[2][2], x[3][0], x[3][1], x[3][2], x[4][0], x[4][1],
+              x[4][2], x[5][0], x[5][1], x[5][2], tree["SubfindID"][0], tree["SnapNum"][0]])
+
+np.savetxt("halos_three_mergers.txt", all_halos, fmt=["%d", "%d", "%d",
+      "%d", "%d", "%d", "%d", "%d", "%d", "%d", "%d", "%d", "%d", "%d", "%.3f",
+      "%.3f", "%.3f", "%d", "%d"], header="Group halo MW analogue, N mergers, id sub1, id sub2, idsub3, snap sub1, snap sub2, snap sub3, id fp1 max, id fp2 max, id fp3 max, merger rat 1, merger rat2, merger rat3, subfindID, SnapNum0")        
+
+
+ """
